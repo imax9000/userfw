fba987ffb7de0f0bbb555b3c7bd51cdc32f3d0e1
Maxim Ignatenko <gelraen.ua@gmail.com>
2012-07-21 14:48:33 +0300

    Add CMake function for defining modules with custom filename

--- cmake/userfw_module.cmake
+++ cmake/userfw_module.cmake
@@ -14,3 +14,20 @@ function(declare_userfw_module modname)
 	install(CODE "execute_process(COMMAND make install \"KMODDIR=${KMODDIR}\" \"PREFIX=${CMAKE_INSTALL_PREFIX}\"
 		WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\")")
 endfunction(declare_userfw_module)
+
+function(declare_userfw_module_with_name modname filename)
+	set(MAKE_ARGS "")
+	if (NOT OPCODE_VERIFICATION)
+		set(MAKE_ARGS "${MAKE_ARGS} SKIP_OPCODE_VERIFICATION=1")
+	endif (NOT OPCODE_VERIFICATION)
+
+	add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko"
+		COMMAND make ${MAKE_ARGS}
+		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+
+	add_custom_target(userfw_${modname} ALL
+		DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko")
+
+	install(CODE "execute_process(COMMAND make install \"KMODDIR=${KMODDIR}\" \"PREFIX=${CMAKE_INSTALL_PREFIX}\"
+		WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\")")
+endfunction(declare_userfw_module_with_name)
