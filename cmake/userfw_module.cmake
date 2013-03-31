b675cb8371401c9437cae9aae2a08950c11a4993
Maxim Ignatenko <gelraen.ua@gmail.com>
2013-03-31 14:35:33 +0100

    Do not touch source tree while building.

--- cmake/userfw_module.cmake
+++ cmake/userfw_module.cmake
@@ -1,33 +1,21 @@
 function(declare_userfw_module modname)
-	set(MAKE_ARGS "")
-	if (NOT OPCODE_VERIFICATION)
-		set(MAKE_ARGS "${MAKE_ARGS} SKIP_OPCODE_VERIFICATION=1")
-	endif (NOT OPCODE_VERIFICATION)
-
-	add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/userfw_${modname}.ko"
-		COMMAND make ${MAKE_ARGS}
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
-
-	add_custom_target(userfw_${modname} ALL
-		DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/userfw_${modname}.ko")
-
-	install(CODE "execute_process(COMMAND make install \"KMODDIR=${KMODDIR}\" \"PREFIX=${CMAKE_INSTALL_PREFIX}\"
-		WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\")")
+	declare_userfw_module_with_name(${modname} userfw_${modname})
 endfunction(declare_userfw_module)
 
 function(declare_userfw_module_with_name modname filename)
-	set(MAKE_ARGS "")
+	set(MAKE_ARGS "S=${CMAKE_CURRENT_SOURCE_DIR}" "MAKEOBJDIRPREFIX=${CMAKE_BINARY_DIR}")
 	if (NOT OPCODE_VERIFICATION)
-		set(MAKE_ARGS "${MAKE_ARGS} SKIP_OPCODE_VERIFICATION=1")
+		list(APPEND MAKE_ARGS "SKIP_OPCODE_VERIFICATION=1")
 	endif (NOT OPCODE_VERIFICATION)
 
-	add_custom_command(OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko"
+	add_custom_command(OUTPUT "${CMAKE_BINARY_DIR}/${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko"
+		COMMAND make ${MAKE_ARGS} obj
 		COMMAND make ${MAKE_ARGS}
 		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
 
 	add_custom_target(userfw_${modname} ALL
-		DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko")
+		DEPENDS "${CMAKE_BINARY_DIR}/${CMAKE_CURRENT_SOURCE_DIR}/${filename}.ko")
 
-	install(CODE "execute_process(COMMAND make install \"KMODDIR=${KMODDIR}\" \"PREFIX=${CMAKE_INSTALL_PREFIX}\"
+	install(CODE "execute_process(COMMAND make install \"KMODDIR=${KMODDIR}\" \"PREFIX=${CMAKE_INSTALL_PREFIX}\" ${MAKE_ARGS}
 		WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\")")
 endfunction(declare_userfw_module_with_name)
